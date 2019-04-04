62da7293d0b8d6d1296aa592ffb8c055e4e57066
Kevin Soares Correia <kevin.soarescorreia@stud.hslu.ch>
2019-04-04 10:37:04 +0200

    Python Library Test2

--- Main.py
+++ Main.py
@@ -15,12 +15,14 @@ while bool == True:
     print("Drücken Sie 3 + Entertaste um die Anordnungsplattform zu vibrieren")
     print("Drücken Sie 4 + Entertaste um die Prüflingsform zu prüfen")
     print("Drücken Sie 5 + Entertaste umd das Programm zu beenden")
+    print ("-----------" * 6)
+
 
     inputVal = input("Geben Sie eine Option an!")
 
 
     if inputVal == 1:
-        Motoren.anheben1()
+        Motoren.anhebenOben()
     elif inputVal == 2:
         Motoren.vibSortier()
     elif inputVal == 3:
