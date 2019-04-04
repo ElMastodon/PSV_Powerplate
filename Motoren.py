63f44c7d493006ac28bbdb12803cea68bdfa9470
Kevin Soares Correia <kevin.soarescorreia@stud.hslu.ch>
2019-04-04 11:27:20 +0200

    Python Library Test2

--- Motoren.py
+++ Motoren.py
@@ -6,19 +6,19 @@ import time
 # Pin 12 (GPIO17) um Plattform anzuheben
 
 def anhebenOben():
-    try:
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(27, GPIO.OUT)
-        GPIO.setup(17, GPIO.OUT)
-        GPIO.output(27, GPIO.LOW)
-        GPIO.output(17, GPIO.LOW)
 
-        time.sleep(10)
-        GPIO.output(17, GPIO.LOW)
-        GPIO.cleanup()
-    finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(27, GPIO.OUT)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(17, GPIO.LOW)
+
+    time.sleep(10)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.cleanup()
+
 
 def anhebenUnten():
     GPIO.cleanup
