6e0a8b9329a6d5fe410452e062884e071463fcc3
Kevin Soares Correia <kevin.soarescorreia@stud.hslu.ch>
2019-04-04 11:56:18 +0200

    Python Library Test2

--- Motoren.py
+++ Motoren.py
@@ -4,11 +4,23 @@ import time
 
 
 # Pin 12 (GPIO17) um Plattform anzuheben
-
 def init():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(27, GPIO.OUT)
     GPIO.setup(17, GPIO.OUT)
+
+#Motor 2
+def init2():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(10, GPIO.OUT)
+
+#Motor 3
+def init3():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(9, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+
 
 def anhebenOben():
 
@@ -24,13 +36,15 @@ def anhebenOben():
 
 
 def anhebenUnten():
-    GPIO.cleanup
-    GPIO.setmode(GPIO.BOARD)
+    try:
+        GPIO.output(27, GPIO.HIGH)
+        GPIO.output(17, GPIO.LOW)
 
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.output(17, GPIO.LOW)
-    time.sleep(1)
-    GPIO.cleanup
+        time.sleep(10)
+        GPIO.output(27, GPIO.LOW)
+    finally:
+        print("Fertig!")
+        GPIO.cleanup()
 
 
 def vibSortier():
