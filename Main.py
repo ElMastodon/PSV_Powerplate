3c171b15f0d210186cfcc820567657d6f4d35af5
Kevin Soares Correia <kevin.soarescorreia@stud.hslu.ch>
2019-05-16 10:48:04 +0200

    Python Library Test2

--- Main.py
+++ Main.py
@@ -212,7 +212,7 @@ while bool == True:
                 GPIO.setup(7, GPIO.IN)  # Stopp
                 GPIO.setup(23, GPIO.IN)  # Endschalter Schublade offen
                 print("stop gedrückt")
-                Motoren.schubSchliessen(80, 3)
+                Motoren.schubSchliessen(80, secSchub)
                 Motoren.anhebenUnten(40,3)
 
                 GPIO.setmode(GPIO.BCM)
